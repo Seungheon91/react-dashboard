bac9a7f68bd82e635c4ea36a27d2773f839097eb
StarkLSH <seungheon1029@gmail.com>
2023-06-28 23:38:13 +0900

    feat: sidebar, navbar UI 구성

--- src/App.js
+++ src/App.js
@@ -23,8 +23,10 @@ import {
   Editor,
 } from "./pages";
 
+import { useStateContext } from "./contexts/ContextProvider";
+
 function App() {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
